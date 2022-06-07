f0fae38f8a52e3a1a8cbec98a812feb0090cc333
rashidul191 <rashidul.191cse.gub@gmail.com>
2022-06-07 20:27:24 +0600

    create genius-car-services

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Footer = () => {
   const year = new Date().getFullYear();
@@ -7,7 +6,12 @@ const Footer = () => {
     <footer className="App bg-info py-5 text-white mt-4">
       <h6>
         {" "}
-        <small>copyright &copy; {year} <a target="_blank" href="https://github.com/rashidul191">Rashidul </a> </small>
+        <small>
+          copyright &copy; {year}{" "}
+          <a target="_blank" href="https://github.com/rashidul191">
+            Rashidul{" "}
+          </a>{" "}
+        </small>
       </h6>
     </footer>
   );
